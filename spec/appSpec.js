be96b0043ca6a2e2bd5d5a6a6f79d28f1982e5ff
touzani-marwa-vinci <marwa.touzani@student.vinci.be>
2024-11-13 20:00:07 +0100

    oneMinutes

---
+++ spec/appSpec.js
@@ -0,0 +1,77 @@
+import { BerlinClock } from "../src/app.js";
+
+describe("minute line", function () {
+    const clock = new BerlinClock();
+
+
+    it("1MinuteLine should return off when given 0:00", function () {
+        const time = "0:00";
+
+        const result = clock.oneMinuteLine(time);
+        
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
+
+        expect(result).toBe("aucune lampe allumée");
+    });
+
+    it("1MinuteLine should return 1 lampe when given 0:01 ", function () {
+        const time = "0:01";
+
+        const result = clock.oneMinuteLine(time);
+
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
+
+        expect(result).toBe("1ère lampe jaune allumée");
+    });
+
+    it("1MinuteLine should return 2 lampes when given 0:02 ", function () {
+        const time = "0:02";
+
+        const result = clock.oneMinuteLine(time);
+
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
+
+        expect(result).toBe("deux lampes jaune allumée");
+    });
+
+    it("1MinuteLine should return 3 lampes when given 0:03 ", function () {
+        const time = "0:03";
+
+        const result = clock.oneMinuteLine(time);
+
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
+
+        expect(result).toBe("trois lampes jaune allumée");
+    });
+
+    it("1MinuteLine should return 4 lampes when given 0:04 ", function () {
+        const time = "0:04";
+
+        const result = clock.oneMinuteLine(time);
+
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
+
+        expect(result).toBe("quatre lampes jaune allumée");
+    });
+    it("1MinuteLine should return off when given 0:15 ", function () {
+        const time = "0:15";
+
+        const result = clock.oneMinuteLine(time);
+
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
+        
+        expect(result).toBe("aucune lampe allumée");
+    });
+});
---
+++ src/app.js
@@ -0,0 +1,20 @@
+export class BerlinClock {
+
+    oneMinuteLine(time) {
+
+      const minutes = parseInt(time.split(":")[1]);
+
+      if(minutes % 10 ===0 || minutes % 10 === 5)return "aucune lampe allumée";
+      
+      if(minutes % 10 ===1 || minutes % 10 === 6)return "1ère lampe jaune allumée";
+
+      if(minutes % 10 ===2 || minutes % 10 === 7)return "deux lampes jaune allumée";
+
+      if(minutes % 10 ===3 || minutes % 10 === 8)return "trois lampes jaune allumée";
+
+      if(minutes % 10 ===4 || minutes % 10 === 9)return "quatre lampes jaune allumée";
+
+
+    }
+
+}
